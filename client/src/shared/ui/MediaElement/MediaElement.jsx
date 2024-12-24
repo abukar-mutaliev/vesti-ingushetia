87f9b1b6bc6c854bda89ef1e22f063e155c0961e
abukar-mutaliev <abukar.mutaliev.js@gmail.com>
2024-12-24 22:12:31 +0300

    update poster Quality

--- client/src/shared/ui/MediaElement/MediaElement.jsx
+++ client/src/shared/ui/MediaElement/MediaElement.jsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { FaPlayCircle } from 'react-icons/fa';
 import defaultImage from '@assets/default.jpg';
 import { getVideoThumbnailUrl } from '@shared/lib/getVideoThumbnailUrl/getVideoThumbnailUrl.js';
@@ -6,26 +6,42 @@ import PropTypes from 'prop-types';
 import styles from './MediaElement.module.scss';
 
 export const MediaElement = ({
-    imageUrl = null,
-    videoUrl = null,
-    alt,
-    className = '',
-    playIconSize = 30,
-    showPlayIcon = true,
-    onError = () => {},
-}) => {
+                                 imageUrl = null,
+                                 videoUrl = null,
+                                 alt,
+                                 className = '',
+                                 playIconSize = 30,
+                                 showPlayIcon = true,
+                                 onError = () => {},
+                             }) => {
     const hasVideo = useMemo(() => Boolean(videoUrl), [videoUrl]);
+    const [useHighQuality, setUseHighQuality] = useState(true);
 
     const videoPosterUrl = useMemo(() => {
         return hasVideo ? getVideoThumbnailUrl(videoUrl) : null;
     }, [hasVideo, videoUrl]);
 
+    useEffect(() => {
+        if (hasVideo && videoPosterUrl) {
+            const img = new Image();
+            img.src = videoPosterUrl.highQuality;
+        }
+    }, [hasVideo, videoPosterUrl]);
+
     const src = useMemo(() => {
         if (hasVideo && videoPosterUrl) {
-            return videoPosterUrl;
+            return useHighQuality ? videoPosterUrl.highQuality : videoPosterUrl.fallback;
         }
         return imageUrl || defaultImage;
-    }, [hasVideo, videoPosterUrl, imageUrl]);
+    }, [hasVideo, videoPosterUrl, imageUrl, useHighQuality]);
+
+    const handleImageError = (e) => {
+        if (hasVideo && useHighQuality) {
+            setUseHighQuality(false);
+            return;
+        }
+        onError(e);
+    };
 
     return (
         <div className={`${styles.mediaContainer} ${className}`}>
@@ -34,7 +50,7 @@ export const MediaElement = ({
                 alt={alt}
                 className={styles.mediaImage}
                 loading="lazy"
-                onError={onError}
+                onError={handleImageError}
             />
             {hasVideo && videoPosterUrl && showPlayIcon && (
                 <div className={styles.playIcon}>
