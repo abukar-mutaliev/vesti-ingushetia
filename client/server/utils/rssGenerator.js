7328667b7db9678e92f90a3ca51510fd818f4bf7
Abu <abubakr.ingush@gmail.com>
2025-02-14 19:14:03 +0300

    rssGenerator fix path

--- client/server/utils/rssGenerator.js
+++ client/server/utils/rssGenerator.js
@@ -1,3 +1,6 @@
+const { XMLBuilder } = require("fast-xml-parser");
+
+
 const generateRssFeed = (newsItems) => {
     const feed = {
         rss: {
@@ -6,15 +9,15 @@ const generateRssFeed = (newsItems) => {
             "@_xmlns:yandex": "http://news.yandex.ru",
             channel: {
                 title: "Новости ГТРК Ингушетия",
-                link: "https://ingushetiatv.ru:5000/",
+                link: "https://ingushetiatv.ru/",
                 description: "Последние новости с сайта ВЕСТИ ИНГУШЕТИИ",
                 language: "ru",
                 lastBuildDate: new Date().toUTCString(),
                 item: newsItems.map((news) => ({
                     title: news.title,
-                    link: `https://ingushetiatv.ru:5000/api/news/${news.id}`,
+                    link: `https://ingushetiatv.ru:5000/news/${news.id}`,
                     description: news.description,
-                    pubDate: new Date(news.published_at).toUTCString(),
+                    pubDate: new Date(news.publishDate).toUTCString(),
                     "dc:creator": news.author || "Редакция",
                     "yandex:full-text": news.content || "",
                 })),
